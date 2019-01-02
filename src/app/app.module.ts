69256dde905ad95d76cb7da132975ab1961a2f1c
George <gcwebster@hotmail.co.uk>
2019-01-02 16:37:28 +0000

    made routing work like a boss!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,34 @@ import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    ),
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase, 'my-app-name'), // imports firebase/app needed for everything
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
-    AngularFireStorageModule // imports firebase/storage only needed for storage features
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    
   ],
-  declarations: [ AppComponent, LoginComponent ],
+  declarations: [ AppComponent, LoginComponent, RegisterComponent ],
   bootstrap: [ AppComponent ]
 })
+
 export class AppModule {}
+
+
