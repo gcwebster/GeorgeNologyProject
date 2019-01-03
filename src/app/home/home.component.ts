3f9941769ab6bf1ad966cb0206a62b0b750023a9
George <gcwebster@hotmail.co.uk>
2019-01-03 09:29:36 +0000

    can successfully display user email address on welcome screen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { AuthServiceService } from '../services/auth-service.service';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,18 @@ import { Observable } from 'rxjs';
 })
 export class HomeComponent implements OnInit {
 
+  user;
+
   items: Observable<any[]>;
-  constructor(db: AngularFirestore) {
+  constructor(public db: AngularFirestore, public afAuth: AuthServiceService) {
     this.items = db.collection('items').valueChanges();
+    this.user = this.afAuth.user;
+
   }
 
   ngOnInit() {
+    this.user = this.afAuth.user;
+    console.error(this.user.email);
   }
 
 }
