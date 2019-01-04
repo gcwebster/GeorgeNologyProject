4f6334bd63d20ee9ffefa2f5cd1221318393a13d
George <gcwebster@hotmail.co.uk>
2019-01-04 11:24:22 +0000

    card generation works(ish)

--- src/app/letters/letters.component.ts
+++ src/app/letters/letters.component.ts
@@ -11,7 +11,14 @@ import { DatabaseService } from '../services/database.service';
   styleUrls: ['./letters.component.css']
 })
 export class LettersComponent implements OnInit {
-
+  i = -1;
+  christmasCards = [
+    'https://images-na.ssl-images-amazon.com/images/I/61vQuriN1TL._SY355_.jpg',
+    'http://canyouactually.com/wp-content/uploads/5-333.jpg',
+    'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1538574368-MIN-ZRF-HNP-001HOLIDAY_A_PZ.jpg?crop=0.667xw:1xh;center,top&resize=480:*',
+    'https://i.ebayimg.com/images/g/xTcAAOSwa3BaEEh4/s-l300.jpg',
+    'https://www.funkypigeon.com/uimg/christmas04/card_rachhale_xmas17_dachshund_3160_p.jpg'
+  ]
   user;
   presents: Observable<any[]>;
   constructor(public db: AngularFirestore, public afAuth: AuthServiceService, public router: Router, public dbService: DatabaseService) {
@@ -22,10 +29,12 @@ export class LettersComponent implements OnInit {
   ngOnInit() {
   }
 
-  updateLetter(present){
-    this.dbService.updateLetterSent(present).catch((error) =>{
-      console.error(error.message);
-    });;
+  getCardFace(){
+    if(this.i > 3)
+      this.i = -1;
+    this.i++;
+    console.log("value of i is: " + this.i + " card at that position is: " + this.christmasCards[this.i]);
+    return this.christmasCards[this.i];
   }
 
 }
