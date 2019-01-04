01371bab31fb6f48af65fc8099f5d92aece0d721
George <gcwebster@hotmail.co.uk>
2019-01-04 12:37:07 +0000

    major imporvements to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,6 +11,7 @@ import { DatabaseService } from '../services/database.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  avatarClicked = false;
 
   user;
   newItem = '';
@@ -34,6 +35,10 @@ export class HomeComponent implements OnInit {
     this.dbService.user = this.user;
   }
 
+  accessProfilePictureChange(){
+    this.avatarClicked = !this.avatarClicked;
+    this.newImage = '';
+  }
   logout(){
     this.afAuth.logout().then(()=>{
       this.router.navigate(['/login']);
@@ -42,37 +47,45 @@ export class HomeComponent implements OnInit {
 
   createItem(){
     //If statement stops empty submission, try/catch bought in to allow for empty field error that isn't caught from firebase promise.
-    if(this.newItem != ''){
-      try{
-        this.dbService.createItem(this.newItem, this.nameFrom, this.nameTo, this.picture, this.rating, this.user.uid).then(()=>{
-          this.newItem = '';
-          this.nameFrom = '';
-          this.nameTo = '';
-          this.picture = '';
-          this.rating= 0;
-          this.errorMessage = '';
-        })
-        .catch((error)=>{
+    if(this.newItem != '' && this.nameFrom != '' && this.nameTo != '' && this.picture != ''){
+      if(this.picture.indexOf('http') != -1){
+        try{
+          this.dbService.createItem(this.newItem, this.nameFrom, this.nameTo, this.picture, this.rating, this.user.uid).then(()=>{
+            this.newItem = '';
+            this.nameFrom = '';
+            this.nameTo = '';
+            this.picture = '';
+            this.rating= 0;
+            this.errorMessage = '';
+          })
+          .catch((error)=>{
+            this.errorMessage = error.message;
+            console.error(this.errorMessage);
+          });
+        }
+        catch (error){
           this.errorMessage = error.message;
-          console.error(this.errorMessage);
-        });
+        }
       }
-      catch (error){
-        if(error.message == "Function DocumentReference.set() called with invalid data. Unsupported field value: undefined (found in field rating)")
-          this.errorMessage = "You didn't fill out all the fields, double check they're all complete!"
-        else
-          this.errorMessage = error.message;
-      }
+      else
+        this.errorMessage = 'This isn\'t a valid URL, make sure it starts with "http" or "https"';
     }
+    else
+      this.errorMessage = "You didn't fill out all the fields, double check they're all complete!";
   }
 
   updateProfilePicture(){
-    this.afAuth.setUserPhotoURL(this.user.displayName, this.newImage).then(()=>{
-      this.newImage = '';
-    })
-    .catch((error)=> {
-      this.errorUpdateImage = error.message;
-    })
+    if(this.newImage.indexOf('http') != -1){
+      this.afAuth.setUserPhotoURL(this.user.displayName, this.newImage).then(()=>{
+        this.newImage = '';
+        this.errorUpdateImage = '';
+      })
+      .catch((error)=> {
+        this.errorUpdateImage = error.message;
+      })
+    }
+    else
+      this.errorUpdateImage = 'This isn\'t a valid URL, make sure it starts with "http" or "https"';
 
   }
 
